79222d086f8ae91a9c20b9c42697e68b0b0c798d
maikeljh <mjh191003@gmail.com>
2023-12-23 15:11:56 +0700

    chore: remove unused libraries

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { query, update, Canister, text, Record, StableBTreeMap, Ok, None, Some, Err, Vec, Result, nat64, ic, Opt, Variant } from 'azle';
+import { query, update, Canister, text, Record, StableBTreeMap, Ok, Err, Vec, Result, nat64, ic, Variant } from 'azle';
 import { v4 as uuidv4 } from 'uuid';
 
 /**
